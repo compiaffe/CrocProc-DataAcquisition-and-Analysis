7f7096b5c96dae76e8e30c144072b2c184badb29
Raffael Santi <compiaffe@web.de>
2013-04-03 18:33:20 +0100

    weka jRip rules implemented

--- pure_derivative.ino
+++ pure_derivative.ino
@@ -43,16 +43,38 @@
 #define DATA_READY_PRESSURE 0x4
 
 
+#define s1 data_buffer[0]
+#define s2 data_buffer[1]
+#define s3 data_buffer[2]
+#define s4 data_buffer[3]
+#define s5 data_buffer[4]
+#define s6 data_buffer[5]
+#define s7 data_buffer[6]
+#define s8 data_buffer[7]
+#define s9 data_buffer[8]
+#define s10 data_buffer[9]
+#define s11 data_buffer[10]
+#define s12 data_buffer[11]
+#define s13 data_buffer[12]
+#define Yes 1
+#define No 0
+
+
 
 //the raw input value in binary
 struct triple{
-  unsigned long s0;
-  unsigned long s1;
-  unsigned long s2;
+  unsigned long s_0;
+  unsigned long s_1;
+  unsigned long s_2;
   unsigned long t;
 
 } 
 tri_raw;
+
+unsigned long s0_log[13];
+unsigned long s1_log[13];
+unsigned long s2_log[13];
+
 
 unsigned int last_raw = 0;
 int derivative;
@@ -130,32 +152,45 @@ void setup()
 /******************** VOID Loop ******************************************/
 
 void loop(){
+  int x;
 #ifdef DEBUG
   Serial.println("Hello World!");
 #endif
 
   Soft_TripleBarometerRead(&tri_raw);
+
   /*Find the derivative and take the absolute value of that*/
-  s0_dev = tri_raw.s0-s0_old;
+  s0_dev = tri_raw.s_0-s0_old;
   s0_dev = abs(s0_dev);
 
-  s1_dev = tri_raw.s1-s1_old;  
+  s1_dev = tri_raw.s_1-s1_old;  
   s1_dev = abs(s1_dev);
 
-  s2_dev = tri_raw.s2-s2_old;  
+  s2_dev = tri_raw.s_2-s2_old;  
   s2_dev = abs(s2_dev);
+  /********************************************************/
 
-  s0_old = tri_raw.s0;
-  s1_old = tri_raw.s1;
-  s2_old = tri_raw.s2;
+  s0_old = tri_raw.s_0;
+  s1_old = tri_raw.s_1;
+  s2_old = tri_raw.s_2;
 
+  //store the last 13 measurements
+  for(x = 12; x>0;x--){
+    s0_log[x] = s0_log[x-1];
+    s1_log[x] = s1_log[x-1];
+    s2_log[x] = s2_log[x-1];
 
-  if(s0_dev >= S0_THRES){
+  }
+  s0_log[0] = tri_raw.s_0;
+  s1_log[0] = tri_raw.s_1;
+  s2_log[0] = tri_raw.s_2;
+
+  /******************* Send those timestamps ***************/
+  if(s0_wave(s0_log)){
     Serial.print('0'); 
     Serial.print(' '); 
     Serial.print(tri_raw.t,DEC);
     Serial.println(""); 
-
   }
 
   if(s1_dev >= S1_THRES){
@@ -173,6 +208,7 @@ void loop(){
 
   }
 
+  /********************************************************/
   //  Serial.print(tri_raw.t,DEC);
   //  Serial.print(' ');
   //  Serial.print(tri_raw.s0,DEC);
@@ -181,19 +217,8 @@ void loop(){
   //  Serial.print(' ');
   //  Serial.print(tri_raw.s2,DEC);
   //  Serial.println(" ");
-
-
-  //****************************   measure the time taken... ******************
-  /* old_time = new_time;
-   new_time = micros();
-   Serial.print("us: ");
-   Serial.println((new_time-old_time),DEC);*/
-
-  /* find the derivative.....*/
-  //last_raw = raw;
-  // derivative = raw-last_raw;
-
 }
+
 
 
 unsigned int Soft_TripleBarometerRead(triple *tri_raw){
@@ -325,7 +350,7 @@ unsigned int Soft_TripleBarometerRead(triple *tri_raw){
   //CHANGED: we ignore the fraction now...
   //Serial.println(((MSB_Data<<10)|(CSB_Data<<2)|LSB_Data>>6),DEC);
 
-  tri_raw->s0 = (unsigned long)((Data[2]<<10)|(Data[1]<<2)|(Data[0]>>6));   //all output data put together. 
+  tri_raw->s_0 = (unsigned long)((Data[2]<<10)|(Data[1]<<2)|(Data[0]>>6));   //all output data put together. 
 
 #ifdef DEBUG
   Serial.println("read 2nd data ");
@@ -341,7 +366,7 @@ unsigned int Soft_TripleBarometerRead(triple *tri_raw){
   Data[1] = i2c_1.read(ACK);
   Data[0] = i2c_1.read(NACK);
   i2c_1.stop();
-  tri_raw->s1 = (unsigned long)((Data[2]<<10)|(Data[1]<<2)|(Data[0]>>6));   //all output data put together. 
+  tri_raw->s_1 = (unsigned long)((Data[2]<<10)|(Data[1]<<2)|(Data[0]>>6));   //all output data put together. 
 
 #ifdef DEBUG
   Serial.println("read 3rd data ");
@@ -359,12 +384,249 @@ unsigned int Soft_TripleBarometerRead(triple *tri_raw){
 
   i2c_2.stop();
 
-  tri_raw->s2 = (unsigned long)((Data[2]<<10)|(Data[1]<<2)|(Data[0]>>6));   //all output data put together. 
+  tri_raw->s_2 = (unsigned long)((Data[2]<<10)|(Data[1]<<2)|(Data[0]>>6));   //all output data put together. 
+
+}
+
+int s0_wave(unsigned long data_buffer[13]){
+
+  if((s8 >= 0.094754) && (s5 >= 0.114064) && (s7 >= 0.093751)){
+    return Yes;
+  };
+  if((s12 >= 0.116425) && (s10 >= 0.090428) && (s12 >= 0.157013)){
+    return Yes;
+  }
+  if((s1 >= 0.118435) && (s8 >= 0.07827) && (s7 >= 0.090501) && (s11 <= 0.049196)){
+    return Yes;
+  }
+  if((s1 >= 0.107618) && (s5 >= 0.136535) && (s3 >= 0.039022) ){
+    return Yes;
+  }
+  if((s12 >= 0.084135) && (s10 >= 0.041254) && (s9 <= 0.035335) && (s11 >= 0.081759) && (s9 >= 0.00658)){
+    return Yes;
+  }
+  if((s12 >= 0.047582) && (s7 >= 0.086261) && (s10 >= 0.056804) && (s8 >= 0.097458) && (s11 >= 0.040183) && (s4 >= 0.043103)){
+    return Yes;
+  }
+  if((s1 >= 0.094786) && (s10 >= 0.034625) && (s10 <= 0.081555) && (s7 >= 0.066904) && (s3 >= 0.075982)){
+    return Yes;
+  }
+  if((s11 >= 0.047361) && (s5 >= 0.189817) && (s3 >= 0.084195)){
+    return Yes;
+  }
+  if((s10 >= 0.032316) && (s1 >= 0.081805) && (s10 <= 0.081286) && (s11 >= 0.032578) && (s10 >= 0.058271) && (s3 >= 0.024199) && (s9 <= 0.04701)){
+    return Yes;
+  }
+  if((s11 >= 0.071506) && (s5 <= 0.005717) && (s13 >= 0.072689) && (s6 >= 0.028328)){
+    return Yes;
+  }
+  if((s12 >= 0.045506) && (s10 >= 0.032316) && (s6 <= 0.042945) && (s6 >= 0.02809) && (s4 <= 0.034184) && (s13 <= 0.052485) && (s1 >= 0.019355)){
+    return Yes;
+  }
+  if((s1 >= 0.116402) && (s1 >= 0.16853) && (s5 >= 0.064349)){ 
+    return Yes;
+  }
+  if((s11 >= 0.06454) && (s11 >= 0.243182)){ 
+    return Yes;
+  }
+  if((s2 >= 0.086105) && (s5 <= 0.023498) && (s6 <= 0.043011) && (s6 >= 0.034493)){ 
+    return Yes;
+  }
+  if((s8 >= 0.169368) && (s12 >= 0.125961)){ 
+    return Yes;
+  }
+  if((s11 >= 0.06454) && (s7 >= 0.24207)){ 
+    return Yes;
+  }
+  if((s1 >= 0.088711) && (s1 <= 0.090114)){ 
+    return Yes;
+  }
+  if((s11 >= 0.065417) && (s4 <= 0.0403) && (s9 <= 0.02564) && (s9 >= 0.019506) && (s2 >= 0.065039)){ 
+    return Yes;
+  }
+  if((s1 >= 0.12426) && (s10 >= 0.034625) && (s10 <= 0.036097)){ 
+    return Yes;
+  }
+  if((s9 >= 0.168145) && (s4 >= 0.155626)){ 
+    return Yes;
+  }
+  else{
+    return No;
+  }
 
 }
 
 
+int s1_wave(unsigned long data_buffer[13]){
 
+
+  if((s8 >= 0.094754) && (s5 >= 0.114064) && (s7 >= 0.093751) && (s4 >= 0.038799) ){ 
+    return Yes;
+  } 
+  if((s12 >= 0.116425) && (s10 >= 0.090428) && (s12 >= 0.157013) ){ 
+    return Yes;
+  }
+  if((s1 >= 0.118435) && (s8 >= 0.07827) && (s7 >= 0.090501) && (s11 <= 0.049196) ){ 
+    return Yes;
+  } 
+  if((s1 >= 0.107618) && (s5 >= 0.136535) && (s3 >= 0.039022) ){ 
+    return Yes;
+  }
+  if((s12 >= 0.084135) && (s10 >= 0.041254) && (s9 <= 0.035335) && (s11 >= 0.081759) && (s9 >= 0.00658) ){ 
+    return Yes;
+  }
+  if((s12 >= 0.047582) && (s7 >= 0.086261) && (s10 >= 0.056804) && (s8 >= 0.097458) && (s11 >= 0.040183) && (s4 >= 0.043103) ){ 
+    return Yes;
+  } 
+  if((s1 >= 0.094786) && (s10 >= 0.034625) && (s10 <= 0.081555) && (s7 >= 0.066904) && (s3 >= 0.075982) ){
+    return Yes;
+  } 
+  if((s11 >= 0.047361) && (s5 >= 0.189817) && (s3 >= 0.084195) ){ 
+    return Yes;
+  } 
+  if((s10 >= 0.032316) && (s1 >= 0.081805) && (s10 <= 0.081286) && (s11 >= 0.032578) && (s10 >= 0.058271) && (s3 >= 0.024199) && (s9 <= 0.04701) ){ 
+    return Yes;
+  } 
+  if((s11 >= 0.071506) && (s5 <= 0.005717) && (s13 >= 0.072689) && (s6 >= 0.028328) && (s9 <= 0.044201) ){ 
+    return Yes;
+  } 
+  if((s12 >= 0.045506) && (s10 >= 0.032316) && (s6 <= 0.042945) && (s6 >= 0.02809) && (s4 <= 0.034184) && (s13 <= 0.052485) && (s1 >= 0.019355) ){ 
+    return Yes;
+  } 
+  if((s1 >= 0.16853) && (s5 >= 0.064349) && (s7 >= 0.046448) ){ 
+    return Yes;
+  } 
+  if((s11 >= 0.06454) && (s11 >= 0.243182) ){ 
+    return Yes;
+  } 
+  if((s2 >= 0.086105) && (s5 <= 0.023498) && (s6 <= 0.043011) && (s6 >= 0.034493) ){ 
+    return Yes;
+  } 
+  if((s8 >= 0.169368) && (s12 >= 0.125961) ){ 
+    return Yes;
+  } 
+  if((s11 >= 0.06454) && (s7 >= 0.24207) ){ 
+    return Yes;
+  } 
+  if((s1 >= 0.088711) && (s1 <= 0.090114) ){ 
+    return Yes;
+  }
+  if((s11 >= 0.06454) && (s9 <= 0.02564) && (s9 >= 0.021842) && (s2 >= 0.065039) ){ 
+    return Yes;
+  } 
+  if((s1 >= 0.12426) && (s10 >= 0.034625) && (s10 <= 0.036097) ){
+    return Yes;
+  } 
+  if((s12 >= 0.068161) && (s10 >= 0.065693) && (s6 <= 0.038789) && (s6 >= 0.025867) && (s10 <= 0.080494) && (s9 <= 0.038529) ){ 
+    return Yes;
+  } 
+  if((s9 >= 0.168145) && (s4 >= 0.155626) ){ 
+    return Yes;
+  } 
+  if((s10 >= 0.054349) && (s10 <= 0.055448) ){ 
+    return Yes;
+  }
+  else{
+    return No;
+  }
+
+}
+
+int s2_wave(unsigned long data_buffer[13]){
+
+  if((s8 >= 0.094754) && (s5 >= 0.114064) && (s7 >= 0.093751) && (s4 >= 0.038799) ){ 
+    return Yes;
+  }
+
+  if((s12 >= 0.116425) && (s10 >= 0.090428) && (s12 >= 0.157013) ){ 
+    return Yes;
+  }
+
+  if((s1 >= 0.118435) && (s8 >= 0.07827) && (s7 >= 0.090501) && (s11 <= 0.049196) ){ 
+    return Yes; 
+  }
+
+  if((s1 >= 0.107618) && (s5 >= 0.136535) && (s3 >= 0.039022) ){ 
+    return Yes; 
+  }
+
+  if((s12 >= 0.084135) && (s10 >= 0.041254) && (s9 <= 0.035335) && (s11 >= 0.081759) && (s9 >= 0.00658) ){ 
+    return Yes; 
+  }
+
+  if((s12 >= 0.047582) && (s7 >= 0.086261) && (s10 >= 0.056804) && (s8 >= 0.097458) && (s11 >= 0.040183) && (s4 >= 0.043103) ){ 
+    return Yes; 
+  }
+
+  if((s1 >= 0.094786) && (s10 >= 0.034625) && (s10 <= 0.081555) && (s7 >= 0.066904) && (s3 >= 0.075982) ){ 
+    return Yes; 
+  }
+
+  if((s11 >= 0.047361) && (s5 >= 0.189817) && (s3 >= 0.084195) ){ 
+    return Yes; 
+  }
+
+  if((s10 >= 0.032316) && (s1 >= 0.081805) && (s10 <= 0.081286) && (s11 >= 0.032578) && (s10 >= 0.058271) && (s3 >= 0.024199) && (s9 <= 0.04701) ){ 
+    return Yes; 
+  }
+
+  if((s11 >= 0.071506) && (s5 <= 0.005717) && (s13 >= 0.072689) && (s6 >= 0.028328) && (s9 <= 0.044201) ){ 
+    return Yes; 
+  }
+
+  if((s12 >= 0.045506) && (s10 >= 0.032316) && (s6 <= 0.042945) && (s6 >= 0.02809) && (s4 <= 0.034184) && (s13 <= 0.052485) && (s1 >= 0.019355) ){ 
+    return Yes; 
+  }
+
+  if((s1 >= 0.16853) && (s5 >= 0.064349) && (s7 >= 0.046448) ){ 
+    return Yes; 
+  }
+
+  if((s11 >= 0.06454) && (s11 >= 0.243182) ){ 
+    return Yes; 
+  }
+
+  if((s2 >= 0.086105) && (s5 <= 0.023498) && (s6 <= 0.043011) && (s6 >= 0.034493) ){ 
+    return Yes; 
+  }
+
+  if((s8 >= 0.169368) && (s12 >= 0.125961) ){ 
+    return Yes; 
+  }
+
+  if((s11 >= 0.06454) && (s7 >= 0.24207) ){ 
+    return Yes; 
+  }
+
+  if((s1 >= 0.088711) && (s1 <= 0.090114) ){ 
+    return Yes; 
+  }
+
+  if((s11 >= 0.06454) && (s9 <= 0.02564) && (s9 >= 0.021842) && (s2 >= 0.065039) ){ 
+    return Yes; 
+  }
+
+  if((s1 >= 0.12426) && (s10 >= 0.034625) && (s10 <= 0.036097) ){ 
+    return Yes; 
+  }
+
+  if((s12 >= 0.068161) && (s10 >= 0.065693) && (s6 <= 0.038789) && (s6 >= 0.025867) && (s10 <= 0.080494) && (s9 <= 0.038529) ){ 
+    return Yes; 
+  }
+
+  if((s9 >= 0.168145) && (s4 >= 0.155626) ){ 
+    return Yes; 
+  }
+
+  if((s10 >= 0.054349) && (s10 <= 0.055448) ){ 
+    return Yes; 
+  }
+  else{
+    return No;
+  } 
+
+
+}
 
 
 
