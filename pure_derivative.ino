4ed5ce333c32e7ac64768f949825a1c358476138
Raffael Santi <compiaffe@web.de>
2013-04-04 22:57:24 +0100

    pressure reading works

--- pure_derivative.ino
+++ pure_derivative.ino
@@ -31,7 +31,7 @@
 #define CTRL_REG1 0x26 
 #define PT_DATA_CFG 0x13
 #define set_OST 0x02
-#define set_OST_OS 0x02 //1 OS rate
+#define set_OST_OS 0x3A //128 OS rate
 //0x02 -  1xOS: 90-154Hz
 //0x0A -  2xOS: 70Hz
 //0x12 -  4xOS: 45Hz
@@ -41,26 +41,6 @@
 //#define set_OST_OS 0x3A
 #define set_PDEFE_DREM 0x3
 #define DATA_READY_PRESSURE 0x4
-
-
-#define s1 data_buffer[0]
-#define s2 data_buffer[1]
-#define s3 data_buffer[2]
-#define s4 data_buffer[3]
-#define s5 data_buffer[4]
-#define s6 data_buffer[5]
-#define s7 data_buffer[6]
-#define s8 data_buffer[7]
-#define s9 data_buffer[8]
-#define s10 data_buffer[9]
-#define s11 data_buffer[10]
-#define s12 data_buffer[11]
-#define s13 data_buffer[12]
-#define Yes 1
-#define No 0
-
-
-
 
 //the raw input value in binary
 struct triple{
@@ -72,9 +52,7 @@ struct triple{
 } 
 tri_raw;
 
-unsigned long s0_log[13];
-unsigned long s1_log[13];
-unsigned long s2_log[13];
+
 
 
 unsigned int last_raw = 0;
@@ -86,10 +64,20 @@ unsigned int new_time = 0;
 /*Derivative and threshold variables*/
 long s0_dev, s1_dev,s2_dev;
 long s0_old, s1_old, s2_old;
-#define S0_THRES 60
-#define S1_THRES 50
-#define S2_THRES 50
 
+/*Pressure reading*/
+unsigned long p_ref[3] = {
+  101325,101325,101325}; //std athmosperic pressure
+float g = 9.81;
+float ro_water = 1000; //density of 1 m^3 of water
+
+struct d_trip{
+  double s_0;
+  double s_1;
+  double s_2;
+
+} 
+depth;
 
 /*Set Soft I2C pins */
 const uint8_t SDA_PIN_0 = A4;
@@ -108,6 +96,7 @@ SoftI2cMaster i2c_2(SCL_PIN_2, SDA_PIN_2);
 /********************FUNCTION PROTOTYPES************************************/
 
 unsigned int Soft_TripleBarometerRead(triple *tri_raw);
+void find_depth();
 
 /******************** VOID SETUP ******************************************/
 void setup()
@@ -115,18 +104,6 @@ void setup()
   Serial.begin(57600);  // start serial for output
 
   /******************** Soft I2C *******************************************/
-
-  //  while (!Serial);
-  //
-  //  if (!digitalRead(SDA_PIN_1) && !digitalRead(SCL_PIN_1) && !digitalRead(SDA_PIN_2) && !digitalRead(SCL_PIN_2) && !digitalRead(SCL_PIN_0) && !digitalRead(SCL_PIN_0)) {
-  //    Serial.println("External pull-up resistors appear to be missing.");
-  //    Serial.println("Many false responses may be detected.");
-  //    Serial.println("Type any character to continue.");
-  //
-  //    while (!Serial.available());
-  //    Serial.println();
-  //
-  //  }
 
   /*Set up the data ready flags...*/
   i2c_0.start();
@@ -160,64 +137,17 @@ void loop(){
 
   Soft_TripleBarometerRead(&tri_raw);
 
-  /*Find the derivative and take the absolute value of that*/
-  s0_dev = tri_raw.s_0-s0_old;
-  s0_dev = abs(s0_dev);
-
-  s1_dev = tri_raw.s_1-s1_old;  
-  s1_dev = abs(s1_dev);
-
-  s2_dev = tri_raw.s_2-s2_old;  
-  s2_dev = abs(s2_dev);
-  /********************************************************/
-
-  s0_old = tri_raw.s_0;
-  s1_old = tri_raw.s_1;
-  s2_old = tri_raw.s_2;
-
-  //store the last 13 measurements
-  for(x = 12; x>0;x--){
-    s0_log[x] = s0_log[x-1];
-    s1_log[x] = s1_log[x-1];
-    s2_log[x] = s2_log[x-1];
-
-  }
-  s0_log[0] = s0_dev;
-  s1_log[0] = s1_dev;
-  s2_log[0] = s2_dev;
-
-  /******************* Send those timestamps ***************/
-  if(s0_wave(s0_log)){
-    Serial.print('0'); 
-    Serial.print(' '); 
-    Serial.print(tri_raw.t,DEC);
-    Serial.println(""); 
-  }
-
-  if(s1_wave(s1_log)){
-    Serial.print('1'); 
-    Serial.print(' '); 
-    Serial.print(tri_raw.t,DEC);
-    Serial.println(""); 
-  }
-
-  if(s2_wave(s2_log)){
-    Serial.print('2'); 
-    Serial.print(' '); 
-    Serial.print(tri_raw.t,DEC);
-    Serial.println(""); 
-
-  }
+  find_depth();
 
   /********************************************************/
-  //  Serial.print(tri_raw.t,DEC);
-  //  Serial.print(' ');
-  //  Serial.print(tri_raw.s0,DEC);
-  //  Serial.print(' ');
-  //  Serial.print(tri_raw.s1,DEC);
-  //  Serial.print(' ');
-  //  Serial.print(tri_raw.s2,DEC);
-  //  Serial.println(" ");
+  Serial.print(tri_raw.t,DEC);
+  Serial.print(' ');
+  Serial.print(depth.s_0*1000,DEC);
+  Serial.print(' ');
+  Serial.print(depth.s_1*1000,DEC);
+  Serial.print(' ');
+  Serial.print(depth.s_2*1000,DEC);
+  Serial.println(" ");
 }
 
 
@@ -389,181 +319,52 @@ unsigned int Soft_TripleBarometerRead(triple *tri_raw){
 
 }
 
-int s0_wave(unsigned long data_buffer[13]){
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    switch(inChar){
+    case 'r':
+      Soft_TripleBarometerRead(&tri_raw);
+      p_ref[0] = tri_raw.s_0; //find the reference value from the sensors.
+      p_ref[1] = tri_raw.s_1;
+      p_ref[2] = tri_raw.s_2;
+        /********************************************************/
+  Serial.print("New pressure reference: ");
+  Serial.print(' ');
+  Serial.print(p_ref[0],DEC);
+  Serial.print(' ');
+  Serial.print(p_ref[1],DEC);
+  Serial.print(' ');
+  Serial.print(p_ref[2],DEC);
+  Serial.println(" ");
+      break;
 
-  if((s8 >= 43.061788) && (s1 >= 47.044819) && (s7 >= 42.00036)){
-    return Yes;
-  };
-  if((s12 >= 48.0867) && (s10 >= 37.154513) && (s12 >= 67.026308)){
-    return Yes;
+    }
   }
-  if((s5 >= 63.201201) && (s5 >= 85.323909) && (s3 >= 41.605193)){
-    return Yes;
-  }
-  if((s10 >= 17.009657) && (s12 >= 33.008174) && (s7 >= 40.123026) && (s4 >= 19.190769) && (s8 >= 32.954839)){
-    return Yes;
-  }
-  if((s10 >= 16.100443) && (s1 >= 44.075981) && (s1 >= 70.048143) && (s8 >= 36.506475) && (s9 <= 46.481138) && (s7 >= 5.463363)){
-    return Yes;
-  }
-  if((s11 >= 37.259917) && (s9 <= 17.90118) && (s12 >= 40.12363) && (s10 >= 19) && (s5 <= 30.792526) && (s7 <= 44.60942)){
-    return Yes;
-  }
-  if((s1 >= 38.039751) && (s7 >= 40.165526) && (s6 <= 20.591899) && (s6 >= 13.131021) && (s3 >= 34.104776)){
-    return Yes;
-  }
-  if((s11 >= 16.100709) && (s6 <= 20.591899) && (s6 >= 13.067296) && (s1 >= 33.444044) && (s4 <= 24.961308) && (s2 >= 28.130365)){
-    return Yes;
-  }
-  if((s11 >= 37.093031) && (s11 >= 97.198105) && (s11 >= 113.079724)){
-    return Yes;
-  }
-  if((s5 >= 63.90544) && (s7 >= 36.786726) && (s11 >= 14.009874) && (s11 <= 27.929526)){
-    return Yes;
-  }
-  if((s12 >= 22.156163) && (s10 >= 15.027197) && (s9 <= 20.553591) && (s4 <= 17.764133) && (s11 >= 17.452486) && (s3 <= 18.81828) && (s12 <= 28.977502)){
-    return Yes;
-  }
-  if((s12 >= 22.156163) && (s4 >= 100.691997)){ 
-    return Yes;
-  }
-  if((s12 >= 34.145018) && (s6 <= 19.902736) && (s6 >= 13.067296) && (s6 <= 13.951598)){ 
-    return Yes;
-  }
-  if((s5 <= 14.993666) && (s5 >= 14.088654)){ 
-    return Yes;
-  }
-  if((s11 >= 43.934672) && (s5 <= 1.81001) && (s13 >= 33.800366)){ 
-    return Yes;
-  }
-  else{
-    return No;
-  }
-
 }
 
 
-int s1_wave(unsigned long data_buffer[13]){
+void find_depth(){
+  triple sea_level_p;
+  sea_level_p.s_0 = tri_raw.s_0-p_ref[0];
+  sea_level_p.s_1 = tri_raw.s_1-p_ref[1];
+  sea_level_p.s_2 = tri_raw.s_2-p_ref[2];
 
-
-  if((s8 >= 43.061788) && (s1 >= 47.044819) && (s7 >= 42.00036) ){ 
-    return Yes;
-  } 
-  if((s12 >= 48.0867) && (s10 >= 37.154513) && (s12 >= 67.026308)){ 
-    return Yes;
-  }
-  if((s5 >= 63.201201) && (s5 >= 85.323909) && (s3 >= 41.605193)){ 
-    return Yes;
-  } 
-  if((s10 >= 17.009657) && (s12 >= 33.008174) && (s7 >= 40.123026) && (s4 >= 19.190769) && (s8 >= 32.954839)){ 
-    return Yes;
-  }
-  if((s10 >= 16.100443) && (s1 >= 44.075981) && (s1 >= 70.048143) && (s8 >= 36.506475) && (s9 <= 46.481138) && (s7 >= 5.463363)){ 
-    return Yes;
-  }
-  if((s11 >= 37.259917) && (s9 <= 17.90118) && (s12 >= 40.12363) && (s10 >= 19) && (s5 <= 30.792526) && (s7 <= 44.60942) ){ 
-    return Yes;
-  } 
-  if((s1 >= 38.039751) && (s7 >= 40.165526) && (s6 <= 20.591899) && (s6 >= 13.131021) && (s3 >= 34.104776)){
-    return Yes;
-  } 
-  if((s11 >= 16.100709) && (s6 <= 20.591899) && (s6 >= 13.067296) && (s1 >= 33.444044) && (s4 <= 24.961308) && (s2 >= 28.130365) ){ 
-    return Yes;
-  } 
-  if((s11 >= 37.093031) && (s11 >= 97.198105) && (s11 >= 113.079724)){ 
-    return Yes;
-  } 
-  if((s5 >= 63.90544) && (s7 >= 36.786726) && (s11 >= 14.009874) && (s11 <= 27.929526)){ 
-    return Yes;
-  } 
-  if((s12 >= 22.156163) && (s10 >= 15.027197) && (s9 <= 20.553591) && (s4 <= 17.764133) && (s11 >= 17.452486) && (s3 <= 18.81828) && (s12 <= 28.977502)){ 
-    return Yes;
-  } 
-  if((s12 >= 22.156163) && (s4 >= 100.691997)){ 
-    return Yes;
-  } 
-  if((s12 >= 34.145018) && (s6 <= 19.902736) && (s6 >= 13.067296) && (s6 <= 13.951598)){ 
-    return Yes;
-  } 
-  if((s5 <= 14.993666) && (s5 >= 14.088654)){ 
-    return Yes;
-  } 
-  if((s11 >= 43.934672) && (s5 <= 1.81001) && (s13 >= 33.800366)){ 
-    return Yes;
-  }  else{
-    return No;
-  }
-
-}
-
-int s2_wave(unsigned long data_buffer[13]){
-
-  if((s8 >= 43.061788) && (s1 >= 47.044819) && (s7 >= 42.00036)){ 
-    return Yes;
-  }
-
-  if((s12 >= 48.0867) && (s10 >= 37.154513) && (s12 >= 67.026308)){ 
-    return Yes;
-  }
-
-  if((s5 >= 63.201201) && (s5 >= 85.323909) && (s3 >= 41.605193)){ 
-    return Yes; 
-  }
-
-  if((s10 >= 17.009657) && (s12 >= 33.008174) && (s7 >= 40.123026) && (s4 >= 19.190769) && (s8 >= 32.954839)){ 
-    return Yes; 
-  }
-
-  if((s10 >= 16.100443) && (s1 >= 44.075981) && (s1 >= 70.048143) && (s8 >= 36.506475) && (s9 <= 46.481138) && (s7 >= 5.463363)){ 
-    return Yes; 
-  }
-
-  if((s11 >= 37.259917) && (s9 <= 17.90118) && (s12 >= 40.12363) && (s10 >= 19) && (s5 <= 30.792526) && (s7 <= 44.60942)){ 
-    return Yes; 
-  }
-
-  if((s1 >= 38.039751) && (s7 >= 40.165526) && (s6 <= 20.591899) && (s6 >= 13.131021) && (s3 >= 34.104776)){ 
-    return Yes; 
-  }
-
-  if((s11 >= 16.100709) && (s6 <= 20.591899) && (s6 >= 13.067296) && (s1 >= 33.444044) && (s4 <= 24.961308) && (s2 >= 28.130365)){ 
-    return Yes; 
-  }
-
-  if((s11 >= 37.093031) && (s11 >= 97.198105) && (s11 >= 113.079724)){ 
-    return Yes; 
-  }
-
-  if((s5 >= 63.90544) && (s7 >= 36.786726) && (s11 >= 14.009874) && (s11 <= 27.929526)){ 
-    return Yes; 
-  }
-
-  if((s12 >= 22.156163) && (s10 >= 15.027197) && (s9 <= 20.553591) && (s4 <= 17.764133) && (s11 >= 17.452486) && (s3 <= 18.81828) && (s12 <= 28.977502)){ 
-    return Yes; 
-  }
-
-  if((s12 >= 22.156163) && (s4 >= 100.691997)){ 
-    return Yes; 
-  }
-
-  if((s12 >= 34.145018) && (s6 <= 19.902736) && (s6 >= 13.067296) && (s6 <= 13.951598)){ 
-    return Yes; 
-  }
-
-  if((s5 <= 14.993666) && (s5 >= 14.088654)){ 
-    return Yes; 
-  }
-
-  if((s11 >= 43.934672) && (s5 <= 1.81001) && (s13 >= 33.800366)){ 
-    return Yes; 
-  } else{
-    return No;
-  } 
+  //  Pressure = densitiy*g*height...
+  depth.s_0 = sea_level_p.s_0/(ro_water*g);
+  depth.s_1 = sea_level_p.s_1/(ro_water*g);
+  depth.s_2 = sea_level_p.s_2/(ro_water*g);
 
 
 }
-
-
 
 
 
